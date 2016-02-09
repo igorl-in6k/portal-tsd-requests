7d9e255f61c5b6f94c14dece3f4b9ffd980eff19
Igor Lychak <igorl@interlink-ua.com>
2016-02-09 04:39:24 +0200

    remove trash tests

---
+++ app/scripts/controllers/upgrade-request-controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+tsdApp
+  .controller('UpgradeRequestController', ['$cookies','$scope',function($cookies, $scope) {
+
+      $scope.reasonsToReplace = ['Breakdown', 'Upgrade', 'Individual preference', 'Other'];
+      $scope.criticalities = ['Low: nice-to-have', 'Medium: ready to be patient, but not for too long', 'High: blocker for work'];
+
+      $scope.send = function() {
+          $cookies.put('equipment', $scope.equipment);
+          $cookies.put('reason', $scope.selectedReason);
+          $cookies.put('criticality', $scope.selectedCriticality);
+          $cookies.put('additionalInfo', $scope.additionalInfo);
+      };
+
+      $scope.cancel = function() {
+          $scope.equipment =  $scope.additionalInfo = '';
+          $scope.selectedReason = $scope.selectedCriticality = null;
+      };
+
+}]);
