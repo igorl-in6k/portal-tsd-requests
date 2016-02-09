0a541b74537ecb48d854ae3e9b50e08abc772a5f
Igor Lychak <igorl@interlink-ua.com>
2016-02-09 04:42:01 +0200

    add empty AccessRequestController

---
+++ app/scripts/controllers/access-request-controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+tsdApp
+  .controller('AccessRequestController', ['$cookies','$scope',function($cookies, $scope) {
+
+      $scope.send = function() {
+      };
+
+      $scope.cancel = function() {
+      };
+
+}]);
