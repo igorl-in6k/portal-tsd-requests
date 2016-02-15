cd4487744295c511a690cf1fb47c85886b241cc7
Igor Lychak <igorl@interlink-ua.com>
2016-02-15 05:11:13 +0200

    add access-form directive

---
+++ app/scripts/directives/access-form-directive.js
@@ -0,0 +1,25 @@
+'use strict';
+
+tsdApp
+.directive('accessForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/access.html',
+        controller: ['$cookies','$scope',function($cookies, $scope) {
+
+            $scope.send = function() {
+                $cookies.put('access.resourceAdress', $scope.accessRequest.resourceAdress);
+                $cookies.put('access.projectName', $scope.accessRequest.projectName);
+                $cookies.put('access.userLogin', $scope.accessRequest.userLogin);
+                $cookies.put('access.additionalInfo', $scope.accessRequest.additionalInfo);
+            };
+
+            $scope.cancel = function() {
+                 $scope.accessRequest.resourceAdress = $scope.accessRequest.projectName =
+                 $scope.accessRequest.userLogin = $scope.accessRequest.additionalInfo = '';
+                 $scope.haveAccount = false;
+            };
+
+      }]
+    };
+});
