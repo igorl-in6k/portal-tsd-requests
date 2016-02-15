4ad933b7080b4a29071306d556dc616e12d347d1
Igor Lychak <igorl@interlink-ua.com>
2016-02-15 05:07:13 +0200

    add upgrade-form directive

---
+++ app/scripts/directives/upgrade-form-directive.js
@@ -0,0 +1,37 @@
+'use strict';
+
+tsdApp
+.directive('upgradeForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/upgrade.html',
+        controller: ['$cookies','$scope',function($cookies, $scope) {
+
+            $scope.upgradeRequest = {
+                reasonsToReplace: [
+                    'Breakdown',
+                    'Upgrade',
+                    'Individual preference',
+                    'Other'],
+                criticalities: [
+                    'Low: nice-to-have',
+                    'Medium: ready to be patient, but not for too long',
+                    'High: blocker for work'
+                ]
+            };
+
+            $scope.send = function() {
+                $cookies.put('upgrade.equipment', $scope.upgradeRequest.equipment);
+                $cookies.put('upgrade.reason', $scope.upgradeRequest.selectedReason);
+                $cookies.put('upgrade.criticality', $scope.upgradeRequest.selectedCriticality);
+                $cookies.put('upgrade.additionalInfo', $scope.upgradeRequest.additionalInfo);
+            };
+
+            $scope.cancel = function() {
+                $scope.upgradeRequest.equipment = $scope.upgradeRequest.additionalInfo = '';
+                $scope.upgradeRequest.selectedReason = $scope.upgradeRequest.selectedCriticality = null;
+            };
+
+      }]
+    };
+});
